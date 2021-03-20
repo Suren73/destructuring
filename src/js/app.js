73cdee0a40419b148aff28b56dd68df6a09b1e89
Andrei Surkov <andronsurkov73@gmail.com>
2021-03-20 16:46:23 +0300

    Добавил выполненное Домашнее задание

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,15 @@
-const character = {
-	name: 'Лучник',
-	type: 'Bowman',
-	health: 50,
-	level: 3,
-	attack: 40,
-	defence: 10,
-	special: [
-		{
-			id: 8,
-			name: 'Двойной выстрел',
-			icon: 'http://...',
-			description: 'Двойной выстрел наносит двойной урон'
-		},
-		{
-			id: 9,
-			name: 'Нокаутирующий удар',
-			icon: 'http://...'
-			// <- обратите внимание, описание "засекречено"
-		}
-	]
+export default function destructuring({ special }) {
+	const result = [];
+	special.forEach((element) => {
+		const {
+			id, name, icon, description = 'Описание недоступно',
+		} = element;
+		result.push({
+			id,
+			name,
+			icon,
+			description,
+		});
+	});
+	return result;
 }
